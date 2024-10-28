9eb78213fd099e62c8ded9c6b9352928ff5196ed
1sh0zer <s.danila.2116@gmail.com>
2024-10-28 21:02:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from datetime import datetime
 import os
 
 today = datetime.now()
-next = datetime(2025, 10, 10)
+next = datetime(2025, 10, 30)
 delta = next - today
 token = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(token)
-chnl = '@mytestchnl_a'
-bot.edit_message_text(f"Осталось {delta.days}", chat_id=chnl, message_id = 7)
+chnl = '@VoidOpa'
+bot.edit_message_text(f"Осталось {delta.days}", chat_id=chnl, message_id = 186)
